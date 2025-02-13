6b3ba3c26389dae5818a4f51fa2a89cb7be73589
misschampa <jyotikumari9694276043@gmail.com>
2025-02-13 10:47:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,11 +55,11 @@ REF_PREMIUM = 30
 PREMIUM_POINT = 1500
 auth_channel = environ.get('AUTH_CHANNEL', '-1002200165721')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002200165721'))
-request_channel = environ.get('REQUEST_CHANNEL', '-1002200165721')
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002212953349'))
+request_channel = environ.get('REQUEST_CHANNEL', '-1002212953349')
 REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
 UPI_PAY_LOGS = int(environ.get('UPI_PAY_LOGS', '-1002338821560'))
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002200165721'))
+MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '-1002212953349'))
 
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 PORT = os.environ.get('PORT', '5000')
